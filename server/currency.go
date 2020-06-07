06af84c1fd82abe3d0bfb1914acca1649f7ab316
Dawud Ibrahim Ismail <ismaildawud96@gmail.com>
2020-06-07 11:36:01 +0000

    working with bi-directional grpc stream

--- server/currency.go
+++ server/currency.go
@@ -2,9 +2,12 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"github.com/hashicorp/go-hclog"
-	data "github.com/idawud/go-gRpc-microservice/data"
+	"github.com/idawud/go-gRpc-microservice/data"
 	protos "github.com/idawud/go-gRpc-microservice/protos/currency"
+	"io"
+	"time"
 )
 
 type Currency struct {
@@ -22,4 +25,37 @@ func (c *Currency) GetRate( ctx context.Context, rr *protos.RateRequest) (*proto
 		return nil, err
 	}
 	return &protos.RateResponse{Rate: rate}, nil
+}
+
+func (c *Currency) SubscribeRates(src protos.Currency_SubscribeRatesServer) error {
+	// listen for receive stream to process
+	go func() {
+		for {
+			recv, err := src.Recv()
+			if err == io.EOF {
+				c.log.Info("Client connection closed")
+				break
+			}
+
+			if err != nil {
+				c.log.Error("Unable to read from client", "error", err)
+				break
+			}
+			req := fmt.Sprintf("{ Base: %s, Destination: %s}", recv.GetBase().String(), recv.GetDestination().String())
+			c.log.Info("Handle client request", "request", req)
+		}
+	}()
+
+	// server stream every 5sec
+	for  {
+		rate, err := c.rates.GetRate(protos.Currencies_EUR.String(), protos.Currencies_USD.String())
+		if err != nil {
+			return err
+		}
+		err = src.Send(&protos.RateResponse{Rate: rate })
+		if err != nil {
+			return err
+		}
+		time.Sleep(5 * time.Second)
+	}
 }
